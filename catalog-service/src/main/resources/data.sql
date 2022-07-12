90fab8dd61da08841bcca2be53be7b1b86d61dbf
JinhoSong <chosunz4@naver.com>
2022-07-12 10:31:03 +0900

    Catalog 모듈

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into catalog(product_id, product_name, stock, unit_price) values ('CATALOG-001','TEST001',100,1500);
+insert into catalog(product_id, product_name, stock, unit_price) values ('CATALOG-002','TEST002',200,2500);
+insert into catalog(product_id, product_name, stock, unit_price) values ('CATALOG-003','TEST003',300,3500);
